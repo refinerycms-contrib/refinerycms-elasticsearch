a2c9c4b5ae2dee42b3e86da71c81228d386054a3
Paul Hendrick <paul@errorstudio.co.uk>
2014-08-21 17:29:38 +0100

    Use rails 4 route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Refinery::Core::Engine.routes.draw do
 
   # Frontend routes
   namespace :elasticsearch, :path => '' do
-    match "/search", :to => 'search#show', :as => 'search'
+    get '/search', to: 'search#show', as: 'search'
   end
 
 end
